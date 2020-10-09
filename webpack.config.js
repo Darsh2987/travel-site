2314b36c2164bd4fdbee014e9199b611cc3f57ec
Darsh <darsh2987@gmail.com>
2020-10-09 14:48:06 +0100

    added yarn, dependencies and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/assets/js/app.js",
+  output: {
+    filename: "bundled-script.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundled-styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "index",
+      template: "src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets/images", to: "assets/images" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./assets/images",
+              publicPath: "assets/images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./assets/fonts",
+              publicPath: "assets/fonts",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
