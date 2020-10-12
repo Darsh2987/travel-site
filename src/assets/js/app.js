dbb2df9d13768ee2f6542146bc809803521428f2
Darsh <darsh2987@gmail.com>
2020-10-12 19:52:37 +0100

    functionality/styles for mobile nav overlay

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,27 @@
 import "../scss/imports.scss";
+
+window.addEventListener("load", () => {
+  // Global Variables
+  const nav = document.querySelector("#nav");
+  const hamburgerBtn = document.querySelector(".hamburger");
+
+  // Function to remove nav overlay and set hamburger icon to normal when a nav link is clicked
+  function resetNav() {
+    nav.classList.remove("show-nav-mobile");
+    hamburgerBtn.classList.remove("hamburger-cross");
+  }
+
+  // function to show nav overlay when hamburger icon is cliked
+  function hamburger() {
+    document.querySelector("#hamburger").addEventListener("click", () => {
+      if (nav.classList.contains("show-nav-mobile")) {
+        resetNav();
+      } else {
+        nav.classList.add("show-nav-mobile");
+        hamburgerBtn.classList.add("hamburger-cross");
+      }
+    });
+  }
+
+  hamburger();
+});
