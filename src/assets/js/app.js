5c4dbb3d5c56c6b91b970d174919efabd88471d3
Darsh <darsh2987@gmail.com>
2020-10-13 00:30:06 +0100

    nav links functionality and smooth scroll effect

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -23,5 +23,15 @@ window.addEventListener("load", () => {
     });
   }
 
+  // function to call resetNav function and smooth scroll when a nav link is clicked
+  const navLinks = document.querySelectorAll("#nav a");
+  navLinks.forEach((i) => {
+    i.addEventListener("click", (e) => {
+      const el = document.getElementById(e.target.getAttribute("linkHook"));
+      resetNav();
+      el.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
   hamburger();
 });
